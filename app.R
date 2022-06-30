82424a3ea61e6d7a6724dd9615ddae21384fa1c6
Kozeluh <jan.kozeluh@bizztreat.com>
2022-06-30 16:34:05 +0200

    setwd

--- app.R
+++ app.R
@@ -1,6 +1,6 @@
 pacman::p_load(shiny,shinydashboard,dashboardthemes,tidyverse,plotly,forecast)
 
-setwd("/home/kozeluhh/Documents/TreatPre")
+setwd("===your dir===")
 
 source('ui.R')
 source('server.R')
